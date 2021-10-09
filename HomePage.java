b33272c135ac0862d5fb56517383356255277b87
Ankita Dave <ankita_patel141079@yahoo.com>
2021-10-09 14:34:20 +0100

    this is my first commit

---
+++ HomePage.java
@@ -0,0 +1,11 @@
+package com.nopcommerce.demo;
+
+public class HomePage {
+
+    //Main method to execute the program
+    public static void main(String[] args){
+
+        //print statement to print some text.
+        System.out.println("My First Java Program");
+    }
+}
---
+++ VarName.java
@@ -0,0 +1,8 @@
+package com.nopcommerce.demo;
+
+public class VarName  {
+    //instance
+    public void EmployeeId()
+
+}
+
